eee0f48ea4367a7cc4a76bdac95828bc61598113
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-01-08 10:40:31 -0700

    Basic comments

---
+++ Financial Calculator/main.py
@@ -0,0 +1,44 @@
+#Nicholas Larsen 2nd Period Financial Calculator
+
+#What do you want to do?
+print("Hello! Welcome to your financial calculator!\n What would you like to do?\n1: Budget allocator\n2: Compound interest calculator\n3:Saving time\n4:Sales prices\n5:Tip calculator\nChoose:")
+
+#Check my budget allocation
+
+#how much do you make a week?
+#How much do you spend on food per week?
+#How much do you spend on entertainment per week?
+#How much do you spend on home expenses per week?
+#How much do you save per week?
+
+#How long will it take to save?
+
+#How much do you save weekly?
+#How much does what you're saving for cost?
+#savings_cost/weekly_savings
+#print how many weeks it will take
+
+#Calculate sales prices
+
+#How much does the item usually cost?
+#What is the sales tax in your area?
+#How many discounts do you have?
+#How much is it discounted?
+#Calculations
+#Print final price
+
+#Calculate compound interest
+
+#What is your original deposit?
+#What is your yearly interest rate?
+#How long do you wnat to wait?
+#Calculations
+#Print final balance
+
+#Calculate a tip
+
+#How much is the original cost?
+#How much do you want to tip?
+#calculations
+#print final price
+
