962c1434f0dc74c4b7601f4472d66adcab8de3b0
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-01-17 10:51:27 -0700

    g

--- Personal Dictionary/main.py
+++ Personal Dictionary/main.py
@@ -1,49 +1,59 @@
 library = []
 
 #adds new items to the library
-def add():
-    item = input("\nWhat would you like to add to your library?")
-    if item != "e":
-        #!!FIX THIS!!
+def add(library):
+    book = input("\nWhat is the name of the book would you like to add to your library?")
+    if book != "e":
+        author = input("\nWho wrote the book?")
+        pages = input("\nHow many pages long is the book?")
+        genre = input("\nWhat genre is the book?")
+        library.append((book, author, pages, genre))
         print(library)
-        add()
+        add(library)
     else:
         return
 
 #removes items from the library
-def rmove():
+def rmove(library):
     item = input("\nWhat would you like to remove from your library?")
     if item != "e":
-        try:
-            library.remove(item)
-            print(library)
-        except:
-            print("\nSorry, that didn't work.")
-        rmove()
+        for i in library:
+            for attribute in i:
+                if attribute == item:
+                    library.remove(i)
+                    print("Got it!")
+                    rmove(library)
+        print("That book doesn't exist.")
+        rmove(library)
     else:
         return
 
-#searches for items withing the library
-def search():
-    item = input("\nWhat would you like to search for in your library?")
+#searches for items within the library
+def search(library):
+    item = input("\nWhat would you like to search for in your library?(Title, length, author, or genre)")
     if item != 'e':
-        if item in library:
-            print("\nFound it! It's item", (library.index(item)+1), "in your library!")
-        else:
-            print("\nSorry, that doesn't seem to be in your library.")
+        for i in library:
+            for attribute in i:
+                if attribute == item:
+                    print("\nFound it! The book is", i[0], "by", i[1] +',', i[2], "pages long, and a", i[3], "book.\nIt is item", library.index(i)+1, "in your library.")
+                    search(library)
+        print("Sorry, I couldn't find that book.")
+        search(library)
     else:
         return
 
 def main():
     print("\nHello! Welcome to your perosnal library!\nThis program will help you manage a collection of books.\nIf at any point you want to exit, enter 'e'.\n")
     while True:
-        choice = input("\nWhat would you like to do?\n1:Add items to your library.\n2:Remove items from your library.\n3:Search for an item in your library.\nChoose:")
+        choice = input("\nWhat would you like to do?\n1:Add items to your library.\n2:Remove items from your library.\n3:Search for an item in your library.\n4:View Library\nChoose:")
         if choice == '1':
-            add()
+            add(library)
         elif choice == '2':
-            rmove()
+            rmove(library)
         elif choice == '3':
-            search()
+            search(library)
+        elif choice == '4':
+            print(library)
         elif choice == 'e':
             print("\nGoodbye!")
             exit()
