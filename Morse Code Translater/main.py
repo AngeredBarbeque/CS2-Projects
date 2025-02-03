233a6e9f2dad76bdfa45aba815a4c764bd807e5a
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-02-03 22:40:09 +0000

    Added morse to english

--- Morse Code Translater/main.py
+++ Morse Code Translater/main.py
@@ -3,10 +3,20 @@
 english = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
 morse = ['._', '_...', '_._.', '_..', '.', '.._.', '__.', '....', '..', '.___', '_._', '._..', '__', '_.', '___', '.__.', '__._', '._.', '...', '_', '.._', '..._', '.__', '_.._', '_.__', '__..', '.____', '..___', '...__', '...._', '.....', '_....', '__...', '___..', '____.', '_____']
 
-def morse_to_english():
-    message = input("What message would you like to translate?")
-    list_message = message.split()
-    #CONTINUE HERE!!
+def morse_to_english(morse, english):
+    translated = []
+    message = input("What message would you like to translate?\nPlease use . and _, with - surrounded by spaces to represent spaces.\nUse spaces in between letters.\n")
+    list_message = message.split(' ')
+    print(list_message)
+    for i in list_message:
+        for item in morse:
+            if i == item:
+                translated.append(english[morse.index(item)])
+            elif i == '-':
+                translated.append(' ')
+                break
+    translated_str = ''.join(translated)
+    print(translated_str)
 
 
 
@@ -19,7 +29,7 @@ def main():
         if choice == '1':
             break
         elif choice == '2':
-            break
+            morse_to_english(morse, english)
         elif choice == '3':
             print("Goodbye!")
             exit()
