8a37378bf70f1d239b5247f788817fa4699277ff
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-02-24 10:09:07 -0700

    notes

---
+++ Notes/Multi_page/calcs.py
@@ -0,0 +1,11 @@
+def add(num, num_two):
+    return num + num_two
+
+def subtract(num, num_two):
+    return num - num_two
+
+def divide(num, num_two):
+    return num / num_two
+
+def multiply(num, num_two):
+    return num * num_two
