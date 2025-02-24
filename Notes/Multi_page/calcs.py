3d27c28a27cd48905722466c76757c73d5218432
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-02-24 10:47:07 -0700

    Finished to-do

--- Notes/Multi_page/calcs.py
+++ Notes/Multi_page/calcs.py
@@ -1,3 +1,5 @@
+#Nicholas larsen, functions to add, subtract, divide and multiply two numbers
+
 def add(num, num_two):
     return num + num_two
 
@@ -9,3 +11,11 @@ def divide(num, num_two):
 
 def multiply(num, num_two):
     return num * num_two
+
+#two underscores make it the name of the file
+#NEVER WRITE ON THE MAIN PAGE
+if __name__ == 'main':
+    add()
+    subtract()
+    divide()
+    multiply()
