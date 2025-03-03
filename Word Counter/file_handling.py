e924202224fbb7e17dbe91b1cac8203b827d9c4d
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-03-03 10:08:30 -0700

    Word counter

--- Word Counter/file_handling.py
+++ Word Counter/file_handling.py
@@ -1,9 +1,9 @@
-
+#Nicholas Larsen file Handling
 from time_handling import  find_time
 def read_file(filename):
     with open(filename, 'r') as file:
         length = len(file.read().split())
     with open(filename, 'a') as file:
         time = find_time()
-        file.write(f'\n{length} pages\nUpdated:{time}')
+        file.write(f'\n{length} words\nUpdated:{time}')
         return length
