6aec859cec85bc69653d7c4c5519e61b3800696c
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-03-16 16:12:09 -0600

    Syncing because github is garbage

--- Battle Simulator/main.py
+++ Battle Simulator/main.py
@@ -3,8 +3,7 @@ from battle_management import *
 from character_management import *
 import random
 from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
-from InquirerPy.separator import Separator
+
 def main():
     print("Hello! Welcome to your battle simulator! You can use this to create characters for you and your friends, and then have them battle each other!")
     while True:
@@ -14,6 +13,7 @@ def main():
             "Create a character",
             "View characters",
             'Battle',
+            'Remove a character',
             "Exit",
         ],
         default=None,
@@ -24,9 +24,13 @@ def main():
             display_chars()
         elif choice == 'Battle':
             result = battle()
-            char = result[0].pop
+            if not result:
+                continue
+            char = result[0]
             exp_gain = result[1]
             edit_char(char, exp_gain)
+        elif choice == 'Remove a character':
+            remove()
         elif choice == 'Exit':
             exit()
 main()
