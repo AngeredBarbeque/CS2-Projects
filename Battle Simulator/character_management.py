842d453fab72b0e32d5abd454ff0a953c7b8a3f7
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-03-16 16:13:17 -0600

    Syncing because github is garbage

--- Battle Simulator/character_management.py
+++ Battle Simulator/character_management.py
@@ -1,12 +1,16 @@
 
 import csv
 from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
-from InquirerPy.separator import Separator
 #Allows the user to create a character
 def create():
     while True:
+        match = False
         name = input("What is the name of your character?\n")
+        chars = get_chars()
+        for i in chars:
+            if i[0] == 'name':
+                print("Sorry, another character is already named that.\n")
+                continue
         print("What class is your character?\n1:Mumified Dog: Boosted defense\nPreserve: Preserve yourself in a peat bog in order to boost your defense for 3 turns.\nAt Level 5: Arson: launch a flaming ball of swamp matter dealing damage that ignores defense.")
         print("2:Lawyer: Boosted speed\nLawsuit: Sues the oppenent, inflicting them with crippling debt, which deals damage to them every turn.\nAt Level 5: Reverse Jury Nullification: Falsely convicts the oppenent, dealing considerable damage.")
         print("3:Pig: Boosted strength\nCharge: Headbutts the oppenent, dealing damage with a chance to crit.\nAt Level 5: Eat Scraps: Eats a pile of trash in order to regain health.")
@@ -59,7 +63,7 @@ def save_char(name, char_class, level, exp):
         speed = round(speed * 1.5)
     elif char_class == 'Pig':
         strength = round(strength * 1.5)
-    for i in range(level - 1):
+    for i in range(int(level) - 1):
         health = round(health * 1.2)
         strength = round(strength * 1.2)
         defense  =  round(defense * 1.2)
@@ -79,7 +83,7 @@ def display_chars():
 #Returns a list with all the characters
 def get_chars():
     chars = []
-    with open('Battle Simulator/chars.csv', 'r') as file:
+    with open('Battle Simulator\chars.csv', 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
         for i in csv_reader:
@@ -92,45 +96,73 @@ def char_select():
     names = []
     for i in chars:
         names.append(i[0])
-    strchar_one = inquirer.select(
-        message="Select a character:",
-        choices=names.copy(),
-        default=None,
-        ).execute()
-    strchar_two = inquirer.select(
-        message="Select another character:",
-        choices=names.copy(),
-        default=None,
-        ).execute()
-    if strchar_one == strchar_two:
-        print("Please choose two seperate characters.")
-        char_select()
-    else:
-        char_one = []
-        char_two  =  []
-        for i in chars:
-            if i[0] == strchar_one:
-                char_one = i.copy()
-            if i[0] == strchar_two:
-                char_two = i.copy()
-        return [char_one, char_two]
+    try:
+        strchar_one = inquirer.select(
+            message="Select a character:",
+            choices=names.copy(),
+            default=None,
+            ).execute()
+        strchar_two = inquirer.select(
+            message="Select another character:",
+            choices=names.copy(),
+            default=None,
+            ).execute()
+        if strchar_one == strchar_two:
+            print("Please choose two seperate characters.")
+            char_select()
+        else:
+            char_one = []
+            char_two  =  []
+            for i in chars:
+                if i[0] == strchar_one:
+                    char_one = i.copy()
+                if i[0] == strchar_two:
+                    char_two = i.copy()
+            return [char_one, char_two]
+    except:
+        print("Please create more than one character before battling.")
+        return False
     
+#Rewrites the CSV file replacing the old character with the updated character
 def edit_char(char,exp_gain):
     chars = get_chars()
     selected = []
     for i in chars:
         if i[0] != char[0]:
             selected.append(i)
-    with open('Battle Simulator/chars.csv', 'w') as file:
+    with open('Battle Simulator/chars.csv', 'w',newline='') as file:
         csv_writer = csv.writer(file)
+        csv_writer.writerow(['Name','Health','Attack','Defense','Speed','Class','Level','EXP'])
         for i in selected:
             csv_writer.writerow(i)
-        char[7] += exp_gain
-        if char[7] >= 100:
-            char[7] = 0
-            char[6] += 1
-        updated_character = [char[0],char[1],char[2],char[3],char[4],char[5],char[6],char[7]]
-        csv_writer.writerow(updated_character)
+    char[7] = str(int(char[7]) + exp_gain)
+    if int(char[7]) >= 100:
+        char[7] = '0'
+        char[6] = str(int(char[6]) + 1)
+    save_char(char[0],char[5],char[6],char[7])
 
-    
-    
+#Rewrites the CSV file with al but the selected character.
+def delete_char(str_char):
+    chars = get_chars()
+    selected = []
+    for i in chars:
+        if i[0] != str_char:
+            selected.append(i)
+    with open('Battle Simulator/chars.csv', 'w',newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Name','Health','Attack','Defense','Speed','Class','Level','EXP'])
+        for i in selected:
+            csv_writer.writerow(i)
+
+#Allows the user to remove a character
+def remove():
+    chars = get_chars()
+    names = []
+    for i in chars:
+        names.append(i[0])
+    char = inquirer.select(
+    message="Select a character:",
+    choices=names.copy(),
+    default=None,
+    ).execute()
+    delete_char(char)
