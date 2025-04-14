234b5782f8c42a0c66116ec4e396fb816de88f6e
AngeredBarbeque <nick.larsen@ucas-edu.net>
2025-04-14 10:50:31 -0600

    started on geo calc

---
+++ Geometry Calculator/classes.py
@@ -0,0 +1,10 @@
+
+class rect:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+
+class tri:
+    def __init__(self):
+        pass
+        #continue here
